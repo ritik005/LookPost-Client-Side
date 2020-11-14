d0a80067af19b90cbde3f94e921f852b15064176
ritikpy <ritikvarshney2014@gmail.com>
2020-11-14 13:36:08 +0530

    Navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MaterialIcon from "material-icons-react";
+
+const Navbar = () => (
+  <nav className="navbar navbar-dark bg-primary mb-0">
+    <div class="container">
+      <a href="/" className="navbar-brand">
+        LOOKPOST
+      </a>
+    </div>
+  </nav>
+);
+
+export default Navbar;
