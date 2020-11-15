42311c9b5e76f46352b9e33c81b2c220c61de4d3
ritikpy <ritikvarshney2014@gmail.com>
2020-11-15 11:27:28 +0530

    Add container section

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Container.css";
+import Image from "../../images/socialnetworking2.svg";
+
+const Container = () => (
+  <section id="banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <p class="promo-title">Welcome!</p>
+          <p class="text">Dont't <b>Google. </b> Just Ask.</p>
+          <button type="button" class="btn btn-primary">
+            Get Started
+          </button>
+        </div>
+        <div class="col-md-6 text-center">
+          <img src={Image} class="img-fluid" />
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default Container;
