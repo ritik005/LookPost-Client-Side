7aafa84f30f625c4b120ea0ab171ad766d8ca97d
ritikpy <ritikvarshney2014@gmail.com>
2020-11-17 11:52:01 +0530

    modify content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Container from "./components/Container/Container";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Login from "./components/Login/Login";
+
 
 const App = () => {
   return (
+    
     <React.Fragment>
       <Navbar />
       <Container />
+      <AboutUs />
       <Footer />
     </React.Fragment>
+    
   );
 };
 
---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './AboutUs.css';
+
+const AboutUs = ()=> (
+ <section id="about-us">
+  <hr />
+      <div class="container">
+        <h1 class="title">About Us</h1>
+        <div class="row">
+          <div class="col-md-8 about-us">
+            <p>This is a large platform for users who wants guidances for choosing their carreer options. They can apply for the menties and also work with the team on the project handle by any professional.</p>
+          </div>
+          <div class="col-md-4">
+            <button class="btn btn-primary">Read more</button>
+          </div>
+        </div>
+      </div>
+    </section>
+)
+
+export default AboutUs;
