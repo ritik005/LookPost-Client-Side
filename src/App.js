e80bedbcee6b8f3c6b7403a1c620981f0b1ebed1
ritikpy <ritikvarshney2014@gmail.com>
2020-11-23 10:19:48 +0530

    --update: install dependencies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import Container from "./components/Container/Container";
-import AboutUs from "./components/AboutUs/AboutUs";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Landing from "./components/Landing/Landing";
 import Login from "./components/Login/Login";
-
 
 const App = () => {
   return (
-    
-    <React.Fragment>
-      <Navbar />
-      <Container />
-      <AboutUs />
-      <Footer />
-    </React.Fragment>
-    
+    <Router>
+      <Route path="/" exact component={Landing} />
+      <Route path="/login" component={Login} />
+    </Router>
   );
 };
 
