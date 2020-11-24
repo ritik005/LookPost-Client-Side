1b243b7f96e9114346b4836a00cd0611e3302354
ritikpy <ritikvarshney2014@gmail.com>
2020-11-24 09:58:38 +0530

    --modified: modification done!

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import Container from '../Container/Container';
+import AboutUs from '../AboutUs/AboutUs';
+import Footer from '../Footer/Footer';
+
+const Landing = ()=> (
+ <React.Fragment>
+  <Navbar />
+  <Container />
+  <AboutUs />
+  <Footer />
+ </React.Fragment>
+)
+export default Landing;
